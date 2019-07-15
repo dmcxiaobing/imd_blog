fdac9b28ccd8cbde32de093dffb5ce2c1b62d1e8
zhengchuanwei <zhengchuanwei>
2019-07-15 16:31:21 +0800

    add gitignore

--- imd_blog_web/src/main/java/com/david/WebApplication.java
+++ imd_blog_web/src/main/java/com/david/WebApplication.java
@@ -1,4 +1,17 @@
 package com.david;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@EnableFeignClients
+@EnableHystrixDashboard
+@EnableCircuitBreaker
+@SpringBootApplication
 public class WebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebApplication.class, args);
+    }
 }
